8334156b0d329084e1f69a5508cc577e7ba6a6d1
codedubs <35834450+codedubs@users.noreply.github.com>
2018-09-17 01:59:57 -0700

    Checkpoint 10 completed: library

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import albumData from './../data/albums';
+import { Link } from 'react-router-dom';
 
 
+class Library extends Component {
 
-const Library = () => (
-  <section className="library">
-    Library page will go here
-  </section>
-);
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      albums: albumData
+    };
+  }
+
+  render() {
+    return(
+      <section className="library">
+        {
+          this.state.albums.map( (album, index) =>
+              <Link to={`/album/${album.slug}`} key={ index }>
+                <img src={ album.albumCover } alt={ album.title } />
+                <div> { album.title } </div>
+                <div> { album.artist } </div>
+                <div> { album.songs.length } songs </div>
+              </Link>
+          )
+        }
+      </section>
+    );
+  }
+}
 
 
 export default Library;
