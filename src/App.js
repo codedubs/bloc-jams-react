766b62d97cd50ea540a302c50b03802d60130ff2
codedubs <35834450+codedubs@users.noreply.github.com>
2018-09-17 19:40:22 -0700

    assignmnt 4 merged files

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
 import Album from './components/Album';
-<<<<<<< HEAD
 
-=======
->>>>>>> assignment-4-routing
 
 class App extends Component {
   render() {
@@ -23,11 +20,7 @@ class App extends Component {
         <main>
           <Route exact path="/" component={Landing} />
           <Route path="/library" component={Library} />
-<<<<<<< HEAD
           <Route path="/album/:slug" component={Album} />
-=======
-          <Route path="/album" component={Album} />
->>>>>>> assignment-4-routing
         </main>
       </div>
     );
